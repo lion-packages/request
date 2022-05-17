86797b925bdbe29a1021f0fc13e94edc508f21fd
Sergio León <sergioleon4004@hotmail.com>
2022-05-17 11:32:48 -0500

    Feat(Response): New built-in Response class to work with different response requests

--- index.php
+++ index.php
@@ -2,15 +2,25 @@
 
 require_once 'vendor/autoload.php';
 
-use LionRequest\Json;
+use LionRequest\{ Json, Response };
 
-$data = [
-	'name' => "Sergio Leon",
-	'age' => 22
-];
+$data = ['name' => "Sergio Leon", 'age' => 22];
 
 $encode = Json::encode($data);
 $decode = Json::decode($encode);
 
-var_dump($encode);
-var_dump($decode);
+var_dump(
+	Response::success('Efe')
+);
+
+var_dump(
+	Response::error('Efe')
+);
+
+var_dump(
+	Response::warning('Efe')
+);
+
+var_dump(
+	Response::info('Efe')
+);
---
+++ src/LionRequest/Response.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LionRequest;
+
+use LionRequest\Traits\Singleton;
+
+class Response {
+
+	use Singleton;
+
+	public static function response(string $status, ?string $message = null, array|object $data = []): object {
+		return (object) [
+			'status' => $status,
+			'message' => $message,
+			'data' => $data
+		];
+	}
+
+	public static function success(?string $message = null, array|object $data = []): object {
+		return self::response('success', $message, $data);
+	}
+
+	public static function error(?string $message = null, array|object $data = []): object {
+		return self::response('error', $message, $data);
+	}
+
+	public static function warning(?string $message = null, array|object $data = []): object {
+		return self::response('warning', $message, $data);
+	}
+
+	public static function info(?string $message = null, array|object $data = []): object {
+		return self::response('info', $message, $data);
+	}
+
+}
