f380249d00b88f45d8d5c5f6090881e4afba995a
Sergio León <sergioleon4004@hotmail.com>
2022-05-19 09:39:42 -0500

    Refactor(Request): The obtaining of variables in the request method is modified

--- src/LionRequest/Request.php
+++ src/LionRequest/Request.php
@@ -10,7 +10,12 @@ class Request {
 
 	public static function request(): object {
         $content = json_decode(file_get_contents("php://input"), true);
-        return $content === null ? (object) ($_POST + $_FILES + $_GET) : (object) $content;
+
+        if ($content === null) {
+            return (object) [...$_POST, ...$_FILES, ...$_GET];
+        }
+
+        return (object) $content;
     }
 
     public static function env(): object {
