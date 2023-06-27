5de7d537eef417c1ac32f0a63e09aadff0e4c021
Sergio León <sergioleon4004@hotmail.com>
2023-06-27 08:23:17 -0500

    refactor: data type has been changed for functions in LionRequest\Response class

--- src/LionRequest/Response.php
+++ src/LionRequest/Response.php
@@ -20,29 +20,29 @@ class Response {
 		die(Json::encode($response));
 	}
 
-	public static function response(string $status, ?string $message = null, array|object $data = []): object {
+	public static function response(string $status, ?string $message = null, mixed $data = null): object {
 		http_response_code(self::$code);
 
-		if (count($data) > 0) {
+		if ($data != null) {
 			return (object) ['code' => self::$code, 'status' => $status, 'message' => $message, 'data' => $data];
 		}
 
 		return (object) ['code' => self::$code, 'status' => $status, 'message' => $message];
 	}
 
-	public static function success(?string $message = null, array|object $data = []): object {
+	public static function success(?string $message = null, mixed $data = null): object {
 		return self::response('success', $message, $data);
 	}
 
-	public static function error(?string $message = null, array|object $data = []): object {
+	public static function error(?string $message = null, mixed $data = null): object {
 		return self::response('error', $message, $data);
 	}
 
-	public static function warning(?string $message = null, array|object $data = []): object {
+	public static function warning(?string $message = null, mixed $data = null): object {
 		return self::response('warning', $message, $data);
 	}
 
-	public static function info(?string $message = null, array|object $data = []): object {
+	public static function info(?string $message = null, mixed $data = null): object {
 		return self::response('info', $message, $data);
 	}
 
