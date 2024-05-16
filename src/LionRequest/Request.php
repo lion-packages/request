4cc0fbe002a8d4a169c1830b5dfa2e766f0b8b8c
Sleon4 <sergioleon4004@hotmail.com>
2024-05-15 22:34:54 -0500

    feat: Http class has been added with its constants

--- src/LionRequest/Request.php
+++ src/LionRequest/Request.php
@@ -12,137 +12,11 @@ namespace Lion\Request;
 class Request
 {
     /**
-     * [Continue]
-     *
-     * @const HTTP_CONTINUE
-     */
-    const HTTP_CONTINUE = 100;
-
-    /**
-     * [Switching Protocols]
-     *
-     * @const HTTP_SWITCHING_PROTOCOLS
-     */
-    const HTTP_SWITCHING_PROTOCOLS = 101;
-
-    /**
-     * [OK]
-     *
-     * @const HTTP_OK
-     */
-    const HTTP_OK = 200;
-
-    /**
-     * [Created]
-     *
-     * @const HTTP_CREATED
-     */
-    const HTTP_CREATED = 201;
-
-    /**
-     * [Accepted]
-     *
-     * @const HTTP_ACCEPTED
-     */
-    const HTTP_ACCEPTED = 202;
-
-    /**
-     * [No Content]
-     *
-     * @const HTTP_NO_CONTENT
-     */
-    const HTTP_NO_CONTENT = 204;
-
-    /**
-     * [Moved Permanently]
-     *
-     * @const HTTP_MOVED_PERMANENTLY
-     */
-    const HTTP_MOVED_PERMANENTLY = 301;
-
-    /**
-     * [Found]
-     *
-     * @const HTTP_FOUND
-     */
-    const HTTP_FOUND = 302;
-
-    /**
-     * [See Other]
-     *
-     * @const HTTP_SEE_OTHER
-     */
-    const HTTP_SEE_OTHER = 303;
-
-    /**
-     * [Not Modified]
-     *
-     * @const HTTP_NOT_MODIFIED
-     */
-    const HTTP_NOT_MODIFIED = 304;
-
-    /**
-     * [Bad Request]
-     *
-     * @const HTTP_BAD_REQUEST
-     */
-    const HTTP_BAD_REQUEST = 400;
-
-    /**
-     * [Unauthorized]
-     *
-     * @const HTTP_UNAUTHORIZED
-     */
-    const HTTP_UNAUTHORIZED = 401;
-
-    /**
-     * [Forbidden]
-     *
-     * @const HTTP_FORBIDDEN
-     */
-    const HTTP_FORBIDDEN = 403;
-
-    /**
-     * [Not Found]
-     *
-     * @const HTTP_NOT_FOUND
-     */
-    const HTTP_NOT_FOUND = 404;
-
-    /**
-     * [Method Not Allowed]
-     *
-     * @const HTTP_METHOD_NOT_ALLOWED
-     */
-    const HTTP_METHOD_NOT_ALLOWED = 405;
-
-    /**
-     * [Internal Server Error]
-     *
-     * @const HTTP_INTERNAL_SERVER_ERROR
-     */
-    const HTTP_INTERNAL_SERVER_ERROR = 500;
-
-    /**
-     * [Not Implemented]
-     *
-     * @const HTTP_NOT_IMPLEMENTED
-     */
-    const HTTP_NOT_IMPLEMENTED = 501;
-
-    /**
-     * [Service Unavailable]
-     *
-     * @const HTTP_SERVICE_UNAVAILABLE
-     */
-    const HTTP_SERVICE_UNAVAILABLE = 503;
-
-    /**
      * Get all values sent via an HTTP request
      *
      * @return object
      */
-	public static function capture(): object
+    public static function capture(): object
     {
         $content = json_decode(file_get_contents('php://input'), true);
 
@@ -152,9 +26,9 @@ class Request
     /**
      * Is used to send a raw HTTP header
      *
-     * @param  string $header [This parameter represents the name of the header
+     * @param string $header [This parameter represents the name of the header
      * to be sent]
-     * @param  string $value [This parameter represents the value of the header
+     * @param string $value [This parameter represents the value of the header
      * to be sent]
      *
      * @return void
